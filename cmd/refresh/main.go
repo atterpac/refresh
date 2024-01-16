33035325aaa18cd690b1832026257a58c5dfb7f2
atterpac <michael@atterpac.dev>
2024-01-15 19:03:35 -0700

    breakout killprocess

--- cmd/refresh/main.go
+++ cmd/refresh/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	var version string = "0.4.24"
+	var version string = "0.4.25"
 
 	var rootPath string
 	var execCommand string
---
+++ engine/kill_darwin.go
@@ -0,0 +1,29 @@
+//go:build darwin
+package engine
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"syscall"
+	"time"
+)
+
+// Kill spawned child process
+func killProcess(process *os.Process) bool {
+	slog.Info("Killing process", "pid", process.Pid)
+	// Windows requires special handling due to calls happening in "user mode" vs "kernel mode"
+	// User mode doesnt allow for killing process so the work around currently is running taskkill command in cmd
+	pgid, err := syscall.Getpgid(process.Pid)
+	if err != nil {
+		slog.Error(fmt.Sprintf("Getting process group id: %s", err.Error()))
+		return false
+	}
+	err = syscall.Kill(-pgid, syscall.SIGTERM)
+	if err != nil {
+		slog.Error(fmt.Sprintf("Killing process: %s", err.Error()))
+		return false
+	}
+	time.Sleep(250 * time.Millisecond)
+	return true
+}
