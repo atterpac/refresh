31486c1519c724c1b08cf5ff5c60efbe398ae112
atterpac <capretta.michael@gmail.com>
2024-03-16 19:45:53 -0600

    fix windows setpgid

--- engine/process_unix.go
+++ engine/process_unix.go
@@ -4,8 +4,59 @@ package engine
 
 import (
 	"log/slog"
+	"os"
 	"syscall"
+	"time"
 )
+
+func (e *Engine) StartProcesses() {
+	pm := e.ProcessManager
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	if len(pm.processes) == 0 {
+		slog.Warn("No Processes to Start")
+		return
+	}
+	slog.Warn("Starting Processes", "count", len(pm.processes))
+	for _, p := range pm.processes {
+		if p.Exec == "KILL_STALE" {
+			continue
+		}
+		if !firstRun && p.Background {
+			slog.Debug("Leaving background process running", "exec", p.Exec)
+			continue
+		}
+		slog.Debug("Starting Process", "exec", p.Exec, "blocking", p.Blocking, "primary", p.Primary, "background", p.Background, "firstRun", firstRun)
+		cmd := generateExec(p.Exec)
+		p.cmd = cmd
+		if p.Primary {
+			if !firstRun {
+				pm.KillProcesses(true)
+				slog.Debug("Processes killed")
+				time.Sleep(100 * time.Millisecond)
+			}
+			if !e.Config.externalSlog {
+				cmd.Stderr = os.Stderr
+				e.ProcessLogPipe, _ = cmd.StdoutPipe()
+				go printSubProcess(e.ProcessLogPipe)
+			}
+		}
+		var err error
+		if p.Blocking {
+			err = cmd.Run()
+		} else {
+			cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+			err = cmd.Start()
+			p.pgid, _ = syscall.Getpgid(cmd.Process.Pid)
+			go func() {
+				cmd.Wait()
+			}()
+		}
+		if err != nil {
+			slog.Error("Running Command", "exec", p.Exec, "err", err)
+		}
+	}
+}
 
 func (pm *ProcessManager) KillProcesses(ignoreBackground bool) {
 	slog.Debug("Killing Processes")
--- engine/process_windows.go
+++ engine/process_windows.go
@@ -4,9 +4,58 @@ package engine
 
 import (
 	"log/slog"
+	"os"
 	"os/exec"
 	"strconv"
+	"time"
 )
+
+func (e *Engine) StartProcesses() {
+	pm := e.ProcessManager
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	if len(pm.processes) == 0 {
+		slog.Warn("No Processes to Start")
+		return
+	}
+	slog.Warn("Starting Processes", "count", len(pm.processes))
+	for _, p := range pm.processes {
+		if p.Exec == "KILL_STALE" {
+			continue
+		}
+		if !firstRun && p.Background {
+			slog.Debug("Leaving background process running", "exec", p.Exec)
+			continue
+		}
+		slog.Debug("Starting Process", "exec", p.Exec, "blocking", p.Blocking, "primary", p.Primary, "background", p.Background, "firstRun", firstRun)
+		cmd := generateExec(p.Exec)
+		p.cmd = cmd
+		if p.Primary {
+			if !firstRun {
+				pm.KillProcesses(true)
+				slog.Debug("Processes killed")
+				time.Sleep(100 * time.Millisecond)
+			}
+			if !e.Config.externalSlog {
+				cmd.Stderr = os.Stderr
+				e.ProcessLogPipe, _ = cmd.StdoutPipe()
+				go printSubProcess(e.ProcessLogPipe)
+			}
+		}
+		var err error
+		if p.Blocking {
+			err = cmd.Run()
+		} else {
+			err = cmd.Start()
+			go func() {
+				cmd.Wait()
+			}()
+		}
+		if err != nil {
+			slog.Error("Running Command", "exec", p.Exec, "err", err)
+		}
+	}
+}
 
 // Window specific kill process
 func (pm *ProcessManager) KillProcesses(ignoreBackground bool) {
